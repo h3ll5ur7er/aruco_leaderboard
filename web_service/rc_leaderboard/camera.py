8798222980540135a671e210095d60091375200e
Emanuel Knecht <h3ll5ur7er@users.noreply.github.com>
2019-07-02 13:22:47 +0200

    Update camera.py

--- web_service/rc_leaderboard/camera.py
+++ web_service/rc_leaderboard/camera.py
@@ -4,7 +4,12 @@ from .framework import Singleton
 
 class Camera(metaclass=Singleton):
     def __init__(self):
-        self._cap = cv2.VideoCapture()
+        self._cap = cv2.VideoCapture(0)
     def grab(self):
         success, frame = self._cap.read()
         return frame if success else None
+
+    def save(self, path):
+        frame = self.grab()
+        if frame is not None:
+            cv2.imwrite(path, frame)
